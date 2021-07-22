9c01335deb450a12312869e3f00c4762a7b9193f
Ксениза <Kseniza>
2021-07-22 20:23:00 +0300

    Initial commit

---
+++ src/main/java/lesson1/Main.java
@@ -0,0 +1,13 @@
+package lesson1;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = (a + b);
+        if (c >= 0)
+        System.out.println("Сумма положительная");
+        else
+            System.out.println("Сумма отрицательная");
+    }
+}
