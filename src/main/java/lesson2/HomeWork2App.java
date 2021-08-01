9bade7be335861656523a18ed76b0b2c9c8679cc
Ксениза <Kseniza>
2021-08-01 10:06:12 +0300

    готово, я полагаю)

--- src/main/java/lesson2/HomeWork2App.java
+++ src/main/java/lesson2/HomeWork2App.java
@@ -4,6 +4,8 @@ public class HomeWork2App {
     public static void main(String[] args) {
         isSummaIn(1, 4);
         checkNumPositive(-2);
+        isNegativeNumber(-5);
+        numberOfString(5, "Добрый день");
     }
 
     public static void isSummaIn(int a, int b) {
@@ -19,20 +21,37 @@ public class HomeWork2App {
             case 17:
             case 18:
             case 19:
-            case 20:
-            {
+            case 20: {
                 System.out.println("True");
-                break;}
+                break;
+            }
             default:
                 System.out.println("False");
-            }
         }
+    }
     public static void checkNumPositive(int a) {
         if (a >= 0)
             System.out.println("Число положительное");
         else {
             System.out.println("Число отрицательное");
         }
+    }
+    public static void isNegativeNumber(int k) {
+        if (k < 0) {
+           System.out.println("True");
+        }
+        else {
+           System.out.println("False");
 
+
+
+        }
+    }
+    public static void numberOfString(int j, String str) {
+        for (int i = 0; i < j; i++) {
+            System.out.println(str);
+        }
+    }
 }
+
 
