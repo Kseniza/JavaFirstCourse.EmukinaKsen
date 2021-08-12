c68061f1b5d0caaedda301eb9279ec4d02a4ce8c
Ксениза <Kseniza>
2021-08-12 21:17:07 +0300

    добавила паузы, тк неприятно когда так быстро меняются картинки, однако такой код самостоятеьно мне пока не написать. поэтому копипаста.

--- src/main/java/lesson3/HomeWork3App.java
+++ src/main/java/lesson3/HomeWork3App.java
@@ -4,21 +4,74 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class HomeWork3App {
-    public static void main(String args[])  {
-            int[] arr = { 1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
-            for (int i = 0; i < arr.length; i++) {
-            if ( arr[i] == 0) {
-                arr[i] = 1; }
-            else {
-                arr[i] = 0;      }
+    public static void main(String args[]) {
+        int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        if (arr[0] == 0) {
+            arr[0] = 1;
+        } else {
+            arr[0] = 0;
+        }
 
-            }
+        if (arr[1] == 0) {
+            arr[1] = 1;
+        } else {
+            arr[1] = 0;
+        }
+
+        if (arr[2] == 0) {
+            arr[2] = 1;
+        } else {
+            arr[2] = 0;
+        }
+
+        if (arr[3] == 0) {
+            arr[3] = 1;
+        } else {
+            arr[3] = 0;
+        }
+
+        if (arr[4] == 0) {
+            arr[4] = 1;
+        } else {
+            arr[4] = 0;
+        }
+
+        if (arr[5] == 0) {
+            arr[5] = 1;
+        } else {
+            arr[5] = 0;
+        }
+
+        if (arr[6] == 0) {
+            arr[6] = 1;
+        } else {
+            arr[6] = 0;
+        }
+
+        if (arr[7] == 0) {
+            arr[7] = 1;
+        } else {
+            arr[7] = 0;
+        }
+
+        if (arr[8] == 0) {
+            arr[8] = 1;
+        } else {
+            arr[8] = 0;
+        }
+
+        if (arr[9] == 0) {
+            arr[9] = 1;
+        } else {
+            arr[9] = 0;
+        }
+
         System.out.println(Arrays.toString(arr));
 
 
         int[] arry = new int[100];
-        for (int i=0; i < arry.length; i++) {
-            arry[i] = i+1;
+        for (int i = 0; i < arry.length; i++) {
+            arry[i] = i + 1;
         }
         System.out.println(Arrays.toString(arry));
 
